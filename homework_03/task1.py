b763d8ce5324caad6f9820474ce5bac51e072616
Northbreez <Chesden84@mail.ru>
2024-12-21 13:47:41 +0300

    hw3_task1 - complite

--- homework_03/task1.py
+++ homework_03/task1.py
@@ -13,3 +13,26 @@
 # Старый список видеокарт: [ 3070 2060 3090 3070 3090 ]
 # Новый список видеокарт: [ 3070 2060 3070 ]
 
+videoСardsNumber = int(input('Введите количество видеокарт: '))
+videoСards = []
+newVideoCardsList = []
+maxItem = 0
+
+for item in range(videoСardsNumber):
+    videoСards.append(int(input(f'Видеокарта {item + 1}: ')))
+    if videoСards[item] > maxItem:
+        maxItem = videoСards[item]
+for item in range(videoСardsNumber):
+    if videoСards[item] != maxItem:
+        newVideoCardsList.append(videoСards[item])
+
+print()
+print('Старый список видеокарт: [', end=' ')
+for item in range(videoСardsNumber):
+    print(videoСards[item], end=' ')
+print(']')
+
+print('Новый список видеокарт: [', end=' ')
+for item in range(len(newVideoCardsList)):
+    print(newVideoCardsList[item], end=' ')
+print(']')
